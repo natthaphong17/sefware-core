d82f35ac5432c0f4fe97315a3f2b795f6012ef3d
toey <jadsada.luechai@gmail.com>
2018-01-04 12:57:14 +0700

    department update

---
+++ src/app/setup/department/department-dialog/department-dialog.component.ts
@@ -0,0 +1,105 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import {Language} from 'angular-l10n';
+import {TdLoadingService} from '@covalent/core';
+import * as _ from 'lodash';
+import { Department } from '../department';
+import { DepartmentService } from '../department.service';
+import { UploadService } from '../../../services/upload.service';
+
+@Component({
+  selector: 'app-department-dialog',
+  templateUrl: './department-dialog.component.html',
+  styleUrls: ['./department-dialog.component.scss']
+})
+export class DepartmentDialogComponent implements OnInit {
+
+  data: Department = new Department({});
+  error: any;
+  images = [];
+  storage_ref = '/main/settings/department';
+
+  constructor(@Inject(MAT_DIALOG_DATA) public md_data: Department,
+              private _departmentService: DepartmentService,
+              private _loadingService: TdLoadingService,
+              public dialogRef: MatDialogRef<DepartmentDialogComponent>) {
+
+    try {
+      if (md_data) {
+        this.data = new Department(md_data);
+      } else {
+        this._departmentService.requestData().subscribe(() => {
+          this.generateCode();
+        });
+      }
+    } catch (error) {
+      this.error = error;
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  generateCode() {
+    this._loadingService.register('data.form');
+    const prefix = 'Departmwnt';
+    this.data.code = prefix + '-001';
+    console.log('Prev Code :' + this.data.code);
+    this._departmentService.requestLastData().subscribe((s) => {
+      s.forEach((ss: Department) => {
+        // tslint:disable-next-line:radix
+        const str = parseInt(ss.code.substring(ss.code.length - 3, ss.code.length)) + 1;
+        let last = prefix + '-' + str;
+
+        if (str < 100) {
+          last = prefix + '-0' + str;
+        }
+
+        if (str < 10) {
+          last = prefix + '-00' + str;
+        }
+
+        this.data.code = last;
+      });
+      this._loadingService.resolve('data.form');
+    });
+  }
+
+  saveData(form) {
+
+    if (form.valid) {
+
+      this.error = false;
+      this._loadingService.register();
+
+      this.data.name1 = form.value.name1 ? form.value.name1 : null;
+
+      if (this.md_data) {
+        if (_.isEqual(this.data, this.md_data)) {
+          this.dialogRef.close(false);
+        } else {
+          this._departmentService.updateData(this.data).then(() => {
+            this.dialogRef.close(this.data);
+            this._loadingService.resolve();
+          }).catch((err) => {
+            this.error = err.message;
+            this._loadingService.resolve();
+          });
+        }
+      } else {
+        this._departmentService.addData(this.data).then(() => {
+          this.dialogRef.close(this.data);
+          this._loadingService.resolve();
+        }).catch((err) => {
+          this.error = err.message;
+          this._loadingService.resolve();
+        });
+      }
+    }
+  }
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
+
+}
